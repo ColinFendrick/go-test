94e2e1f27e9fe36d0be2e14583d4d4cec96ad332
Colin Fendrick <colin.fendrick@gmail.com>
2019-06-06 12:58:17 -0400

    Make language switch a private function

--- hello_test.go
+++ hello_test.go
@@ -11,14 +11,26 @@ func TestHello(t *testing.T) {
 	}
 
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "")
 		want := "Hello, Chris"
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("empty string defaults to 'World'", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Consuela", "Spanish")
+		want := "Hola, Consuela"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Amelie", "French")
+		want := "Bonjour, Amelie"
 		assertCorrectMessage(t, got, want)
 	})
 }
