99e878f91bb2193317ea5dab23755d7cd0d5aaca
Colin Fendrick <colin.fendrick@gmail.com>
2019-06-26 08:14:28 -0400

    Add append

--- array/sum.go
+++ array/sum.go
@@ -1,5 +1,6 @@
 package main
 
+// Sum all numbers in slice
 func Sum(numbers []int) (sum int) {
 	for _, number := range numbers {
 		sum += number
@@ -7,6 +8,10 @@ func Sum(numbers []int) (sum int) {
 	return
 }
 
-func SumAll(numbers ...[]int) (sum []int) {
+// SumAll numbers in slices
+func SumAll(numbersArray ...[]int) (sums []int) {
+	for _, numbers := range numbersArray {
+		sums = append(sums, Sum(numbers))
+	}
 	return
 }
